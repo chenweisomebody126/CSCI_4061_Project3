96a6862bfdd5bac79df6a79b041a3fbe497b5d30
chenweisomebody126 <chenweisomebody@126.com>
2017-04-11 20:48:51 -0500

    Working code for part A and B

--- PartA/mm.h
+++ PartA/mm.h
@@ -6,10 +6,10 @@
 
 #define INTERVAL 0
 #define INTERVAL_USEC 50000
-#define CHUNK_SIZE 1
-#define NUM_CHUNKS 1000
+#define CHUNK_SIZE 64
+#define NUM_CHUNKS 100000
 
-/* TODO - Fill this in */
+
 typedef struct {
   int count;
   int size_of_chunks;
@@ -17,7 +17,7 @@ typedef struct {
   void **memory_ptr; //poiter to array of chunks
 } mm_t;
 
-/* TODO - Implement these in mm.c */
+
 double comp_time(struct timeval time_s, struct timeval time_e);
 int mm_init(mm_t *mm, int num_chunks, int chunk_size);
 void *mm_get(mm_t *mm);
